b874a60d01ba7748baf8d9f319051884c90c01c2
Ciaowilly3 <muscomassi@gmail.com>
2023-01-11 16:02:24 +0100

    primo completamento, ma 3 esercizio da sistemare

---
+++ query.sql
@@ -0,0 +1,39 @@
+1:
+SELECT * 
+FROM `students`
+WHERE YEAR(`date_of_birth`) = "1990";
+
+2:
+SELECT * 
+FROM `courses`
+WHERE `cfu` > 10;
+
+3:
+SELECT * 
+FROM `students`
+WHERE `date_of_birth` < "1993-01-11";
+
+4:
+SELECT * 
+FROM `courses`
+WHERE `period` = "I semestre"
+AND `year` = 1
+
+5:
+SELECT * 
+FROM `exams`
+WHERE HOUR(`hour`) >= 14
+AND `date` = "2020-06-20";
+
+6:
+SELECT * 
+FROM `degrees`
+WHERE `level` != "triennale";
+
+7:
+SELECT COUNT(id) AS num_dipartimenti FROM `departments`;
+
+8:
+SELECT * 
+FROM `teachers`
+WHERE `phone` IS NULL;
